18cd7eacbf4f0cfd75bb66d90ec83a27e38806ae
goni <gonina@gmail.com>
2019-06-19 11:03:07 +0300

    update

--- Goni/read_data_new.py
+++ Goni/read_data_new.py
@@ -18,8 +18,21 @@ class ProcessData:
         # self.result = {}
         # self.metadata = datacont.metadata
 
-    def convert_to_df (self):
+    def convert_fixations_to_df (self):
     """ convert file to readeble df"""
+    df = self.fixations
+    time_periods = pd.DataFrame({'time': pd.date_range(start = df.start.min(), end = df.end.max(), freq = 'S')}) #create all time-stamps
+    df = time_periods.merge(df, how='left', left_on='time', right_on='start').fillna(method='pad') #merge
+    # print (new_eye.iloc[70:150])
+    mask_eye = (df['time'] > df['start']) & (df['time'] < df['end'])
+    df = df.where(mask_eye)
+    df = df.dropna()
+    # print (fixed_eye.iloc[70:150])
+    df.pop('start')
+    df.pop('end')
+    df.index = df_eye['time']
+    df.pop('time')
+    # print (df_eye)
     pass
 
     def concat_df (self):
