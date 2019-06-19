2836cec1b7d60d617f30bc71cb901abc0b5fee59
NitzanAlbeck <nitzan.albeck@gmail.com>
2019-06-19 21:11:01 +0300

    first day really done

--- Nitzan/visualization.py
+++ Nitzan/visualization.py
@@ -28,33 +28,36 @@ def get_from_Goni():
 def get_2d(df):
     raw_x = df['aveH'].to_numpy().ravel()
     raw_y = df['aveV'].to_numpy().ravel()
-    bins = [1080, 1920]
+    bins = [1080/10, 1920/10]
     data_2d, x_bin, y_bin = np.histogram2d(raw_x, raw_y, bins=bins)
     return data_2d
 
 def convert_to_png(fname_photo, new_name):
-    jgp = Image.open (fname_photo) 
+    jpg = Image.open (fname_photo) 
     png = jpg.save(new_name)
     
 def make_heat(data_2d, ph_png):
     heat = sns.heatmap(data_2d,cbar = True, cmap = 'Reds', alpha = 0.5, zorder = 2)
-    im = mpimg.imread(ph_png)
-        
+    map_img = mpimg.imread(ph_png)
+    
+    heat.imshow(map_img,
+          aspect = heat.get_aspect(),
+          extent = heat.get_xlim() + heat.get_ylim(),
+          zorder = 1) #put the map under the heatmap
+    
+    plt.show()
     
     return heat
-
-
 
 if __name__ == "__main__":
     
     df= get_from_Goni()
     # df = gen_data()
     data_2d= get_2d(df)
-    # con_image = mpimg.imread('FB_on_full_screen_Person.jpg')
-    # make_heat(data_2d)
-    fig, ax = plt.subplots()
-    ax.imshow(data_2d, cmap='gray')
-    plt.show()
+    convert_to_png('FB_on_full_screen_Person.jpg', 'Person.png')
+    make_heat(data_2d,'Person.png')
+    # fig, ax = plt.subplots()
+    # ax.imshow(data_2d, cmap='gray')
 
 
 
