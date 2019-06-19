4e3e6032f637b5bb265f1918dc7b348b519f51c7
goni <gonina@gmail.com>
2019-06-19 12:00:16 +0300

    update

--- Goni/read_data_new.py
+++ Goni/read_data_new.py
@@ -17,18 +17,28 @@ class ProcessData:
         self.events = pd.read_csv(events.path)
         self.id_num = fixations.id_num
         self.design = fixations.design
+        self.df_fixations = None
 
     def convert_fixations_to_df (self):
     """ convert file to readeble df"""
-    index = pd.MultiIndex.from_product([[self.id_num], [self.design]],
-                                    names=['ID', 'design']) 
+    # index = pd.MultiIndex.from_product([[self.id_num], [self.design]],
+    #                                 names=['ID', 'design']) 
+    # columns = pd.MultiIndex.from_product([['time', 'condition', 'aveH', 'aveV']],
+    #                                 names = [None])                                
     df = self.fixations
+    df['ID'] = self.id_num
+    df['design'] = self.design
     time_periods = pd.DataFrame({'time': pd.date_range(start = df.startTime.min(), end = df.endTime.max(), freq = 'S')}) #create all time-stamps
     df = time_periods.merge(df, how='left', left_on='time', right_on='start').fillna(method='pad') #merge
     mask = (df['time'] > df['start']) & (df['time'] < df['end'])
     df = df.where(mask)
     df = df.dropna()
     df.drop(['start', 'end'], axis=1
+    df.set_index(['ID', 'design'])
+
+    # self.df_fixations = pd.DataFrame(data, index=index, columns=columns)
+
+    
     
     
 
@@ -37,17 +47,12 @@ class ProcessData:
         # self.df_files = pd.concat ([df_event, df_eye], axis=1, sort = True)
         # self.df_files = self.df_files.dropna()
         
+if __name__ == "__main__":
+    
+    data1 = ProcessData()
+    data1.convert_fixations_to_df()
+    print (data1.df_fixations)
 
-    id_num = '345'
-    design = '1'
-    data = ()
-
-    index = pd.MultiIndex.from_product([[id_num], [design]],
-                                    names=['ID', 'design']) 
-    columns = pd.MultiIndex.from_product([['time', 'condition', 'aveH', 'aveV']],
-                                        names = [None])
-
-    example_data = pd.DataFrame(data, index=index, columns=columns)
     # exmple:
     # def process(self):
     #     """ Mock processing pipeline """
@@ -55,3 +60,4 @@ class ProcessData:
     #     means = [x.mean() for x in self.datacont.data]
     #     self.result['mean'] = means
     #     return self.result
+
