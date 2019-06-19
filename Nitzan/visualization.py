d740290c16cd0fa77e65088318e2a5de30415742
NitzanAlbeck <nitzan.albeck@gmail.com>
2019-06-19 15:07:30 +0300

    updates - histogram

--- Nitzan/visualization.py
+++ Nitzan/visualization.py
@@ -9,8 +9,15 @@ import seaborn as sns
 from params import *
 
 # calling the data
-from multiindex_file import example_data
-data = example_data
+from multiindex_file import gen_data
 
+if __name__ == "__main__":
+    df = gen_data()
 
-#histgraming:
+    raw_x = df['aveV'].to_numpy().ravel()
+    raw_y = df['aveH'].to_numpy().ravel()
+    data_2d, bins_x, bins_y = np.histogram2d(raw_x, raw_y, bins=15)
+    fig, ax = plt.subplots()
+    ax.imshow(data_2d, cmap='gray')
+    plt.show()
+
