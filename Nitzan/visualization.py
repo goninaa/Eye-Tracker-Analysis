b37d5a67a7108b9a875fe2f8a95afa992212e926
NitzanAlbeck <nitzan.albeck@gmail.com>
2019-06-20 13:46:44 +0300

    last update.

--- Nitzan/visualization.py
+++ Nitzan/visualization.py
@@ -9,82 +9,76 @@ from pathlib import Path
 from PIL import Image
 import matplotlib.image as mpimg
 
+class Visual (self, df, reso, cond_dict, pho_dict):
+    ''' Visuaize the Eye tracker Data
+    '''
 
-from params import *
-from multiindex_file import gen_data
-from read_data_new import *
-
-
-def get_from_Goni():
-    fix = Path('CSV/FB/FB_integration_ID_01_design_1_time_04.11.18_11.43_fixations.csv')
-    event = Path('CSV/FB/FB_integration_ID_01_design_1_time_04.11.18_11.43_messages.csv')
-    fix_obj = EyeFile(path=fix, fname=fix.name, id_num='01', design='1', data_type='fixations')
-    event_obj = EyeFile(path=event, fname=event.name, id_num='01', design='1', data_type='events')
-
-    data1 = IdData(fix_obj, event_obj)
-    data1.run()
-
-    fix2 = Path('CSV/FB/FB_integration_ID_02_design_2_time_04.11.18_13.32_fixations.csv')
-    event2 = Path('CSV/FB/FB_integration_ID_02_design_2_time_04.11.18_13.32_messages.csv')
-    fix_obj2 = EyeFile(path=fix2, fname=fix2.name, id_num='02', design='2', data_type='fixations')
-    event_obj2 = EyeFile(path=event2, fname=fix2.name, id_num='02', design='2', data_type='events')
-
-    data2 = IdData(fix_obj2, event_obj2)
-    data2.run()
-  
-    big_data = AllId ([data1.df_id, data2.df_id])
-    big_data.run()
-
-    return big_data.df_all, big_data.cond_dict
-
-def convert_to_df_not_object(df):
-    df.pop('condition')
-    df_new = df.infer_objects()
-    return df_new
-
-
-def make_heatmap(df, ph_png):
-    #creeating 2d density data : 2d histogram
-    raw_x = df['aveH'].to_numpy().ravel()
-    raw_y = df['aveV'].to_numpy().ravel()
-    bins = [1920/10,1080/10]
-    range_bins = [[0,1920] , [0,1080]]
-    data_2d, x_bin, y_bin = np.histogram2d(raw_x, raw_y, bins=bins, range = range_bins)
-    
-    #creating the heatmap over the picture:
-    heat = sns.heatmap(data_2d.T,cbar = True, cmap = 'Reds', alpha = 0.5, zorder = 2)
-    map_img = mpimg.imread(ph_png)
-    
-    heat.imshow(map_img,
-          aspect = heat.get_aspect(),
-          extent = heat.get_xlim() + heat.get_ylim(),
-          zorder = 1) #put the map under the heatmap
+    def __init__ (self):
+        self.df = df
+        self.reso = reso
+        self.cond_dict = cond_dict
+        self.pho_dict = pho_dict
         
-    return heat
+    def convert_to_df_not_object(self):
+        '''
+        takes out the condition as string, leaving only the integer part.
+        then, changes the DF to not be object.
+        '''
+        self.df.pop('condition')
+        self.df = self.df.infer_objects()
 
-def super_grid(df,cond_dict, pho_dict):
-    con_num = len(cond_dict)
-    
-    f, axes = plt.subplots(3, 2, figsize=(9, 9), sharex=True, sharey=True)
-    
-    for ax, cond_key in zip(axes.flat, cond_dict):
-        df_cond = df.loc[lambda df:df['cond_int'] == cond_dict[cond_key]]
-        # df_cond = df.where(mask)
-        cond_pho = pho_dict[cond_key]
-        make_heatmap(df_cond,cond_pho)
-    plt.show()
+    def make_heatmap(self, ph_png,ax):
+        '''
+        creates a heatmap over the photo in the speceafied file name in ph_png.       
+        then, puts it in the spicifeid ax (to be a sbplot)
+        the function as 2 steps: 
+        1. 2 d histogram, using the resolotion (devided by 10 for better visualize)
+        2. heatmap over the map
+        '''
+
+        #creating 2d density data : 2d histogram
+        raw_x = self.df['aveH'].to_numpy().ravel()
+        raw_y = self.df['aveV'].to_numpy().ravel()
+        bins = [self.reso[0]/10,self.reso[1]/10] #change to tuple
+        range_bins = [[0,self.reso[0]] , [0,self.reso[1]]]
+        data_2d, x_bin, y_bin = np.histogram2d(raw_x, raw_y, bins=bins, range = range_bins)
+        
+        #creating the heatmap over the picture:
+        heat = sns.heatmap(data_2d.T,cbar = True, cmap = 'Reds', alpha = 0.5, zorder = 2, ax=ax)
+        
+        map_img = mpimg.imread(ph_png)
+        
+        heat.imshow(map_img,
+            aspect = heat.get_aspect(),
+            extent = heat.get_xlim() + heat.get_ylim(),
+            zorder = 1, #put the map under the heatmap
+            ) 
+            
+        return heat
+
+    def super_grid(self):
+        ''' makes large subplot to each condition 
+        '''
+
+        con_num = len(self.cond_dict)
+        
+        f, axes = plt.subplots(con_num, 1, figsize=(16, 16),sharex = True, sharey=False)
+        
+        for ax, cond_key in zip(axes, self.cond_dict):
+            df_cond = df.loc[lambda self.df:self.df['cond_int'] == self.cond_dict[cond_key]]
+            cond_pho = self.pho_dict[cond_key]
+            self.make_heatmap(df_cond,cond_pho,ax)
+            ax.title.set_text(str(cond_key))
+
+        plt.show()
+
+    def run(self):
+        '''main pipeline'''
+        self.convert_to_df_not_object()
+        self.super_grid()        
+
+
 
 if __name__ == "__main__":
+    pass
     
-    df, cond_dict = get_from_Goni()
-    df = convert_to_df_not_object(df)
-    # make_heatmap(df,'Person.png')
-    super_grid(df,cond_dict,pho_dict)
-    # plt.show()
-
-
-
-
-
-
-
