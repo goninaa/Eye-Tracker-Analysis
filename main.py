6c133969afb96735565e01601c3ad085ca627608
goni <gonina@gmail.com>
2019-06-20 13:28:29 +0300

    update

--- main.py
+++ main.py
@@ -4,11 +4,29 @@ from process_GUI_input import *
 from eye_GUI import *
 
 # yossi
+class EyeTracker:
+    def __init__(self):
+        self.df_list = []
+        pass
 
 # f.eyedict
-for key, value in f.eyedict:
-    data = IdData(key, value)
-    data.run()
+    def data(self):
+        """ creates data frame list"""
+        for key, value in f.eyedict:
+            fix_f, event_f = value.values
+            data = IdData(fix_f, event_f)
+            data.run()
+            self.df_list.append(data.df_id)
+    
+    def big_data(self):
+         """ create one big data frame from all data frames in the list """
+        b_data = AllId (self.df_list)
+        b_data.run()
+
+    
+
+    def run (self):
+        self.data()
 
 
 
